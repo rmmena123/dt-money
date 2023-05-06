27975450e02ce52adf46a796e24632ac91901aa3
Rodrigo Machado Mena <rm.mena86@gmail.com>
2023-05-06 19:37:12 -0300

    #6: Feature - Componente NewTransactionModal com acessibilidade utilizando Radix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
+import { NewTransactionModal } from "../NewTransactionModal";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from "../../assets/logo.svg"
+import * as Dialog from '@radix-ui/react-dialog';
+import logoImg from "../../assets/logo.svg";
 
 export function Header() {
     return (
@@ -7,7 +9,14 @@ export function Header() {
             <HeaderContainer>
                 <HeaderContent>
                     <img src={logoImg} alt="Logo DT Money" />
-                    <NewTransactionButton>Nova transação</NewTransactionButton>
+
+                    <Dialog.Root>
+                        <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                        </Dialog.Trigger>
+
+                        <NewTransactionModal />
+                    </Dialog.Root>
                 </HeaderContent>
             </HeaderContainer>
         </div>
